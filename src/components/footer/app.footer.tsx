5afa1a0ef604e758b6310299557615bffc1241ff
Nguyễn Hữu Lợi <77217656+bossvirus03@users.noreply.github.com>
2023-10-15 20:44:02 +0700

    Design Footer

---
+++ src/components/footer/app.footer.tsx
@@ -0,0 +1,47 @@
+"use client";
+import Container from "@mui/material/Container";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
+import { useHasMounted } from "@/utils/customHook";
+function AppFooter() {
+  const hasMounted = useHasMounted();
+  if (!hasMounted) return <></>;
+  return (
+    <div>
+      <AppBar
+        position="fixed"
+        sx={{ top: "auto", bottom: 0, background: "#f2f2f2" }}
+      >
+        <Container style={{ display: "flex", gap: 10 }}>
+          <AudioPlayer
+            style={{
+              boxShadow: "unset",
+              background: "#f2f2f2",
+            }}
+            autoPlay
+            src="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-9.mp3"
+            onPlay={(e) => console.log("onPlay")}
+            // other props here
+          />
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "start",
+              justifyContent: "center",
+              minWidth: 100,
+              paddingLeft: 45,
+            }}
+          >
+            <div style={{ color: "#ccc" }}>Loi</div>
+            <div style={{ color: "black" }}>to chim</div>
+          </div>
+        </Container>
+      </AppBar>
+    </div>
+  );
+}
+
+export default AppFooter;
