e9fdc0d288175f8ee092c03401664bf8de94597f
Nguyễn Hữu Lợi <77217656+bossvirus03@users.noreply.github.com>
2023-10-22 01:03:02 +0700

    NextJS Server với Route Handler

--- src/components/track/wave.track.tsx
+++ src/components/track/wave.track.tsx
@@ -1,14 +1,17 @@
 "use client";
+import { useSearchParams } from "next/navigation";
 import { useEffect, useRef } from "react";
 import WaveSurfer from "wavesurfer.js";
 function WaveTrack() {
   const containerRef = useRef<HTMLDivElement>(null);
+  const searchParams = useSearchParams();
+  const search = searchParams.get("audio");
   useEffect(() => {
     const wavesurfer = WaveSurfer.create({
       container: containerRef.current!,
       waveColor: "rgb(200, 0, 200)",
       progressColor: "rgb(100, 0, 100)",
-      url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/track_upload/audio_Track/1.mp3`,
+      url: `/api?audio=${search}`,
     });
   }, []);
   return <div ref={containerRef}>wave track</div>;
