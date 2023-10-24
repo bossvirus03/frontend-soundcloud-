b2bdeec3a6865a55c1c1d00a5e77ba964da371e2
Nguyễn Hữu Lợi <77217656+bossvirus03@users.noreply.github.com>
2023-10-24 14:56:29 +0700

    Sử dụng useMemo hook

--- src/components/track/wave.track.tsx
+++ src/components/track/wave.track.tsx
@@ -1,19 +1,27 @@
 "use client";
+import { useWavesurfer } from "@/utils/customHook";
 import { useSearchParams } from "next/navigation";
-import { useEffect, useRef } from "react";
-import WaveSurfer from "wavesurfer.js";
+import { useEffect, useMemo, useRef, useState } from "react";
+
+//Wavesurfer hook
+
 function WaveTrack() {
   const containerRef = useRef<HTMLDivElement>(null);
   const searchParams = useSearchParams();
   const search = searchParams.get("audio");
-  useEffect(() => {
-    const wavesurfer = WaveSurfer.create({
-      container: containerRef.current!,
+  const option = {
+    waveColor: "rgb(200, 0, 200)",
+    progressColor: "rgb(100, 0, 100)",
+    url: `/api?audio=${search}`,
+  };
+  const optionMemo = useMemo(() => {
+    return {
       waveColor: "rgb(200, 0, 200)",
       progressColor: "rgb(100, 0, 100)",
       url: `/api?audio=${search}`,
-    });
+    };
   }, []);
+  const wavesurfer = useWavesurfer(containerRef, optionMemo);
   return <div ref={containerRef}>wave track</div>;
 }
 
--- src/utils/customHook.ts
+++ src/utils/customHook.ts
@@ -1,4 +1,6 @@
 import { useState, useEffect } from "react";
+import { WaveSurferOptions } from "wavesurfer.js";
+import WaveSurfer from "wavesurfer.js";
 export const useHasMounted = () => {
   const [hasMounted, setHasMounted] = useState<boolean>(false);
   useEffect(() => {
@@ -6,4 +8,26 @@ export const useHasMounted = () => {
   }, []);
 
   return hasMounted;
+};
+export const useWavesurfer = (
+  containerRef: React.RefObject<HTMLDivElement>,
+  options: Omit<WaveSurferOptions, "container">
+) => {
+  const [wavesurfer, setWavesurfer] = useState<any>(null);
+  useEffect(() => {
+    if (!containerRef.current) return;
+
+    const ws = WaveSurfer.create({
+      ...options,
+      container: containerRef.current,
+    });
+
+    setWavesurfer(ws);
+
+    return () => {
+      ws.destroy();
+    };
+  }, [options, containerRef]);
+
+  return wavesurfer;
 };
